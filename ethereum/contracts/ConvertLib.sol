ca121450ca7ba19b55bac252eebd7cc841b09183
tawachan <tawachan@pixiv.com>
2019-02-02 17:58:26 +0900

    sollint

--- ethereum/contracts/ConvertLib.sol
+++ ethereum/contracts/ConvertLib.sol
@@ -1,8 +1,8 @@
-pragma solidity >=0.4.25 <0.6.0;
+pragma solidity ^0.5.2;
 
-library ConvertLib{
-	function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
-	{
-		return amount * conversionRate;
-	}
+
+library ConvertLib {
+    function convert(uint amount, uint conversionRate) public pure returns (uint convertedAmount) {
+        return amount * conversionRate;
+    }
 }
--- ethereum/contracts/DappsToken.sol
+++ ethereum/contracts/DappsToken.sol
@@ -4,6 +4,4 @@ pragma solidity ^0.5.2;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 
-contract DappsToken is ERC20 {
-  
-}
+contract DappsToken is ERC20 {}
